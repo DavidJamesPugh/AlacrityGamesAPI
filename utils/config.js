eb951878b3ffd362adf69174284f67d8224f2d07
David Pugh <davidjames.pugh@gmail.com>
2025-09-26 22:43:24 +1200

    Updating

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './inc.env' });
+dotenv.config({ path: './.env' });
 console.log('PORT:', process.env.PORT);
 const { PORT } = process.env;
 const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
