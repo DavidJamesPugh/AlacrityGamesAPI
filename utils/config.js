490b9ca42b502d617abf980e4841ba1e852166bf
David Pugh <davidjames.pugh@gmail.com>
2025-09-26 22:45:39 +1200

    updating

--- utils/config.js
+++ utils/config.js
@@ -5,6 +5,7 @@ console.log('PORT:', process.env.PORT);
 const { PORT } = process.env;
 const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
 
+
 const config = { PORT, MONGODB_URI };
 
 export { config };
